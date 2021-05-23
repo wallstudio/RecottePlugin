1219bbb18b7f448aa6c84af895b913a4772240b2
UpHash <yukawallstudio@gmail.com>
2021-05-24 07:02:01 +0900

    トップレベルWindowの解析

--- TestPlugin/TestPlugin.cpp
+++ TestPlugin/TestPlugin.cpp
@@ -1,10 +1,50 @@
 ﻿#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <thread>
+#include <vector>
+#include <string>
 
+
+struct WindowInfo
+{
+	HWND Handle;
+	std::string ClassName;
+	std::string Text;
+};
+
+std::thread g_Thread;
+std::vector<WindowInfo> g_Winsows = std::vector<WindowInfo>();
 
 extern "C" __declspec(dllexport) void WINAPI OnPluginStart(HINSTANCE handle)
 {
 	MessageBoxA(NULL, "OnPluginStart", "TestPlugin", MB_OK);
+
+	g_Thread = std::thread(
+		[=]() -> void
+		{
+			while (true)
+			{
+
+				//MessageBoxA(NULL, "OnPluginStart", "--------", MB_OK);
+				g_Winsows.clear();
+				EnumWindows(
+					[](HWND win, LPARAM param) -> BOOL
+					{
+						auto className = std::string(256, '\n');
+						GetClassNameA(win, className.data(), className.size());
+						auto text = std::string(256, '\n');
+						GetWindowTextA(win, text.data(), text.size());
+						g_Winsows.push_back({ win, className, text, });
+						if (0 >= className.find("SumireFrameWindow"))
+						{
+							OutputDebugStringA("");
+						}
+						return TRUE;
+					},
+					reinterpret_cast<LPARAM>(handle));
+				
+			}
+		});
 }
 
 extern "C" __declspec(dllexport) void WINAPI OnPluginFinish(HINSTANCE haneld)
