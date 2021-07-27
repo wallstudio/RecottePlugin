5ddde9bb3944d7dcf6a5ce09a509a85879978e4a
UpHash <yukawallstudio@gmail.com>
2021-07-28 04:10:19 +0900

    FileSystemBypass DLL追加

---
+++ FileSystemBypass/FileSystemBypass.cpp
@@ -0,0 +1,13 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+extern "C" __declspec(dllexport) void WINAPI OnPluginStart(HINSTANCE handle)
+{
+	OutputDebugStringW(L"[FileSystemBypass] " __FUNCTION__ L"\n");
+}
+
+extern "C" __declspec(dllexport) void WINAPI OnPluginFinish(HINSTANCE haneld)
+{
+	OutputDebugStringW(L"[FileSystemBypass]" __FUNCTION__ L"\n");
+}
