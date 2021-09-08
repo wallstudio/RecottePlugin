edbbdef03fb4f06224b5abd66e52360a46e26345
UpHash <yukawallstudio@gmail.com>
2021-09-09 03:00:02 +0900

    Unicode（wchar_t）にしてみる

---
+++ link_debug.bat
@@ -0,0 +1,16 @@
+set RECOTTE="C:\Program Files\RecotteStudio"
+set RECOTTE=%RECOTTE:"=%
+
+set CURRENT_DIR=%~dp0
+openfiles > nul
+if errorlevel 1 (
+    PowerShell.exe -Command Start-Process \"%~f0\" -ArgumentList %CURRENT_DIR% -Verb runas
+    exit
+)
+cd %RECOTTE%
+set PROJECT=%1
+set PROJECT=%PROJECT:"=%
+
+cmd /c del /q "d3d11.dll"
+cmd /c mklink "d3d11.dll" "%PROJECT%x64\Debug\RecottePluginFoundation.dll"
+pause
