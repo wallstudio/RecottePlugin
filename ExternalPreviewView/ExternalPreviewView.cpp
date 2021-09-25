bc1e637de9fa01f890f1e1d6c2192c4ca32df0ff
UpHash <yukawallstudio@gmail.com>
2021-09-26 03:54:07 +0900

    プロジェクト作成 ExternalPreviewView

---
+++ ExternalPreviewView/ExternalPreviewView.cpp
@@ -0,0 +1,11 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "../HookHelper/HookHelper.h"
+
+
+extern "C" __declspec(dllexport) void WINAPI OnPluginStart(HINSTANCE handle)
+{
+	auto c = RecottePluginFoundation::Intenal::OverrideIATFunction;
+}
+
+extern "C" __declspec(dllexport) void WINAPI OnPluginFinish(HINSTANCE haneld) {}
