809d03e2edc695e6b1ea2f45899d87e454d8266c
UpHash <yukawallstudio@gmail.com>
2021-09-26 04:22:48 +0900

    Windowを出す

--- ExternalPreviewView/ExternalPreviewView.cpp
+++ ExternalPreviewView/ExternalPreviewView.cpp
@@ -3,9 +3,85 @@
 #include "../HookHelper/HookHelper.h"
 
 
+HWND window;
+
+
+void InitWindow(HINSTANCE hInstance)
+{
+    // Register class
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"ExternalPreviewWindow";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, (LPCTSTR)IDI_APPLICATION);
+    wcex.lpfnWndProc = [](HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) -> LRESULT
+    {
+        if (message == WM_PAINT)
+        {
+            PAINTSTRUCT ps;
+            HDC hdc;
+            hdc = BeginPaint(hWnd, &ps);
+            EndPaint(hWnd, &ps);
+            return 0;
+        }
+        else if (message == WM_DESTROY)
+        {
+            PostQuitMessage(0);
+            return 0;
+        }
+        else
+        {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    };
+    if (!RegisterClassEx(&wcex)) throw std::wstring(L"fail register");
+
+    // Create window
+    RECT rc = { 0, 0, 800, 600 };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    window = CreateWindow(L"ExternalPreviewWindow", L"RecottePlugin",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+        nullptr);
+    if (!window) throw std::wstring(L"fail create window");
+
+    ShowWindow(window, SW_SHOW);
+}
+
+void Render()
+{
+
+}
+
+void MessageLoop()
+{
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            Render();
+        }
+    }
+}
+
 extern "C" __declspec(dllexport) void WINAPI OnPluginStart(HINSTANCE handle)
 {
-	auto c = RecottePluginFoundation::Intenal::OverrideIATFunction;
+    InitWindow(handle);
+    MessageLoop();
+    exit(0);
 }
 
 extern "C" __declspec(dllexport) void WINAPI OnPluginFinish(HINSTANCE haneld) {}
